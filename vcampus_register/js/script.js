2d6ffb877299f3c716665f592ee10691f1259da9
Takia <tangkienan@gmail.com>
2017-09-18 22:45:07 +0700

    vcampus_register

--- vcampus_register/js/script.js
+++ vcampus_register/js/script.js
@@ -1,20 +1,31 @@
-// Shrink the lower navbar when scroll down
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
-});
-
-// Stick the navbar at the top while scrolling
+// // Shrink the lower navbar when scroll down
+// $(window).scroll(function() {
+//   if ($(document).scrollTop() > 60) {
+//     $('nav').addClass('shrink');
+//   } else {
+//     $('nav').removeClass('shrink');
+//   }
+// });
 $(document).ready(function() {
   $(window).scroll(function () {
-    if ($(window).scrollTop() > 280) {
-      $('#navbar').addClass('navbar-fixed');
+    if ($(window).scrollTop() > 55) {
+      $('#header-lower').addClass('header-fixed');
     }
-    if ($(window).scrollTop() < 281) {
-      $('#navbar').removeClass('navbar-fixed');
+    if ($(window).scrollTop() < 56) {
+      $('#header-lower').removeClass('header-fixed');
+    }
+  });
+});
+
+
+
+$(document).ready(function() {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 55) {
+      $('#header-lower').addClass('navbar-shrink').removeClass('navbar-normal');
+    }
+    if ($(window).scrollTop() < 56) {
+      $('#header-lower').removeClass('navbar-shrink').addClass('navbar-normal');
     }
   });
 });
